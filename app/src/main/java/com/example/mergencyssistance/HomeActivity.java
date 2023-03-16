8f826cc3b83bc70b039d83642263b00ea84a189f
KonstantinD98 <107940686+KonstantinD98@users.noreply.github.com>
2023-03-16 18:28:03 +0200

    early commit

--- app/src/main/java/com/example/mergencyssistance/HomeActivity.java
+++ app/src/main/java/com/example/mergencyssistance/HomeActivity.java
@@ -1,8 +1,16 @@
 package com.example.mergencyssistance;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.mergencyssistance.R;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +18,31 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", "").toString();
+        Toast.makeText(getApplicationContext(),"Welcome "+username, Toast.LENGTH_SHORT).show();
+
+        CardView exit = findViewById(R.id.Exit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.apply();
+                startActivity(new Intent(HomeActivity.this,LoginActivity.class));
+            }
+        });
+        CardView doctor = findViewById(R.id.doctors);
+        doctor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.apply();
+                startActivity(new Intent(HomeActivity.this,DoctorActivity.class));
+            }
+        });
+
     }
+
 }
